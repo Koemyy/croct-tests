01473068c214dc4848df661bbe9c35a77633fed2
Francine Nogueira <francine.pns@gmail.com>
2024-06-10 14:30:22 -0300

    Copy example from code generator

--- croct-js-example/HomeHero.js
+++ croct-js-example/HomeHero.js
@@ -1,33 +1,27 @@
-import croct from "@croct/plug";
+import croct from '@croct/plug';
 
 export async function renderHomeHero(container, fallback) {
   // Display a loading message while the content is being fetched
-  container.innerText = "✨ Personalizing...";
+  container.innerText = '✨ Personalizing...';
 
-  const content = await croct.fetch("home-hero@1").catch(() => fallback);
+  const {content} = await croct.fetch('home-hero@1').catch(() => ({content: fallback});
 
   // Remove the loading message
   container.replaceChildren();
 
   // Render the content
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
-  const promotionPopupElement = document.createElement("div");
-  promotionPopupElement.appendChild(
-    document.createTextNode("Id do produto: " + content.promotionPopup.id)
-  );
-  promotionPopupElement.appendChild(
-    document.createTextNode("Título do popup: " + content.promotionPopup.title)
-  );
-  promotionPopupElement.appendChild(
-    document.createTextNode("Cor de fundo: " + content.promotionPopup.bgColor)
-  );
-  promotionPopupElement.appendChild(
-    document.createTextNode("Label: " + content.promotionPopup.ctaTitle)
-  );
-  promotionPopupElement.appendChild(
-    document.createTextNode("Cor do texto: " + content.promotionPopup.textColor)
-  );
+  const promotionPopupTitleElement = document.createElement('strong');
+  promotionPopupTitleElement.innerText = 'Promotion popup';
+  element.appendChild(promotionPopupTitleElement);
+
+  const promotionPopupElement = document.createElement('div');
+  promotionPopupElement.innerText += `Id do produto: ${content.promotionPopup.id}\n`;
+  promotionPopupElement.innerText += `Título do popup: ${content.promotionPopup.title}\n`;
+  promotionPopupElement.innerText += `Cor de fundo: ${content.promotionPopup.bgColor}\n`;
+  promotionPopupElement.innerText += `Label: ${content.promotionPopup.ctaTitle}\n`;
+  promotionPopupElement.innerText += `Cor do texto: ${content.promotionPopup.textColor}\n`;
 
   element.appendChild(promotionPopupElement);
 
